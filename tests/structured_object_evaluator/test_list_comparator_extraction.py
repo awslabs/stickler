526efc693ba56849c2d90ec31fadd218725a1844
Spencer Romo <sromo@amazon.com>
2025-10-08 13:32:32 -0500

    Removing incorrect AWS license terms.

--- tests/structured_object_evaluator/test_list_comparator_extraction.py
+++ tests/structured_object_evaluator/test_list_comparator_extraction.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright © Amazon.com and Affiliates: This deliverable is considered Developed Content as defined in the AWS Service Terms and the SOW between the parties dated 2025.
+
 
 """
 Baseline tests for Hungarian matching extraction.
--- tests/structured_object_evaluator/test_structured_model_simple.py
+++ tests/structured_object_evaluator/test_structured_model_simple.py
@@ -1,4 +1,4 @@
-# Copyright © Amazon.com and Affiliates: This deliverable is considered Developed Content as defined in the AWS Service Terms and the SOW between the parties dated 2025.
+
 
 """Simple test for StructuredModel compare_with functionality."""
 
