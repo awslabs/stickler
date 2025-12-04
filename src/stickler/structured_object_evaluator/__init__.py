6b0559b91e2bcf340d1ac298b049cf20e4bcc40c
Ryan Razkenari <razken@amazon.com>
2025-12-04 06:57:05 -0500

    chore: Clean up unused assets and formatting in package exports

--- src/stickler/structured_object_evaluator/__init__.py
+++ src/stickler/structured_object_evaluator/__init__.py
@@ -9,7 +9,6 @@ from .models.comparable_field import ComparableField
 from .models.non_match_field import NonMatchField, NonMatchType
 from .utils.anls_score import compare_structured_models, anls_score, compare_json
 from .utils.key_scores import ScoreNode, construct_nested_dict, merge_and_calculate_mean
-
 from .utils.pretty_print import print_confusion_matrix, print_confusion_matrix_html
 
 __all__ = [
@@ -23,7 +22,6 @@ __all__ = [
     "ScoreNode",
     "construct_nested_dict",
     "merge_and_calculate_mean",
-
     "print_confusion_matrix",
     "print_confusion_matrix_html",
 ]
